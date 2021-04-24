999d2d35c89fe531e48eb962760c2920cddb784a
Takeru Ohta <phjgt308@gmail.com>
2021-04-24 09:51:32 +0900

    Use thiserror for error handling

--- src/error.rs
+++ src/error.rs
@@ -1,79 +1,27 @@
-use std::io;
-use std::fmt;
-use std::error;
-use std::convert;
 use beam_file;
-use eetf;
+use std::convert;
+use std::fmt;
+use std::io;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum FromBeamError {
-    Io(io::Error),
-    BeamFile(beam_file::Error),
-    TermDecode(eetf::DecodeError),
+    #[error("I/O error")]
+    Io(#[from] io::Error),
+
+    #[error("beam file parse error")]
+    BeamFile(#[from] beam_file::Error),
+
+    #[error("erlang term decoding error")]
+    TermDecode(#[from] eetf::DecodeError),
+
+    #[error("no debug information")]
     NoDebugInfo,
+
+    #[error("no module attribute")]
     NoModuleAttribute,
+
+    #[error("unexpected terms: {:?}", .0.iter().map(|x| x.to_string()).collect::<Vec<_>>().join(","))]
     UnexpectedTerm(Vec<Unmatched>),
-}
-impl error::Error for FromBeamError {
-    fn description(&self) -> &str {
-        match *self {
-            FromBeamError::Io(ref x) => x.description(),
-            FromBeamError::BeamFile(ref x) => x.description(),
-            FromBeamError::TermDecode(ref x) => x.description(),
-            FromBeamError::NoDebugInfo => "No debug information",
-            FromBeamError::NoModuleAttribute => "No module attribute",
-            FromBeamError::UnexpectedTerm(_) => "Unexpected term",
-        }
-    }
-    fn cause(&self) -> Option<&error::Error> {
-        match *self {
-            FromBeamError::Io(ref x) => x.cause(),
-            FromBeamError::BeamFile(ref x) => x.cause(),
-            FromBeamError::TermDecode(ref x) => x.cause(),
-            _ => None,
-        }
-    }
-}
-impl fmt::Display for FromBeamError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            FromBeamError::Io(ref x) => x.fmt(f),
-            FromBeamError::BeamFile(ref x) => x.fmt(f),
-            FromBeamError::TermDecode(ref x) => x.fmt(f),
-            FromBeamError::NoDebugInfo => write!(f, "The beam has no debug information"),
-            FromBeamError::NoModuleAttribute => write!(f, "No module attribute"),
-            FromBeamError::UnexpectedTerm(ref trace) => {
-                try!(write!(f, "Unexpected term: ["));
-                let limit = 3;
-                for (i, e) in trace.iter().take(limit).enumerate() {
-                    if i != 0 {
-                        try!(write!(f, ","));
-                    }
-                    try!(write!(f, "{}", e));
-                }
-                if trace.len() > limit {
-                    try!(write!(f, " ..{}..", trace.len() - limit));
-                }
-                try!(write!(f, "]"));
-                Ok(())
-            }
-        }
-    }
-}
-impl convert::From<io::Error> for FromBeamError {
-    fn from(x: io::Error) -> Self {
-        FromBeamError::Io(x)
-    }
-}
-impl convert::From<beam_file::Error> for FromBeamError {
-    fn from(x: beam_file::Error) -> Self {
-        FromBeamError::BeamFile(x)
-    }
-}
-impl convert::From<eetf::DecodeError> for FromBeamError {
-    fn from(x: eetf::DecodeError) -> Self {
-        FromBeamError::TermDecode(x)
-    }
 }
 impl<'a> convert::From<eetf::pattern::Unmatch<'a>> for FromBeamError {
     fn from(x: eetf::pattern::Unmatch<'a>) -> Self {
